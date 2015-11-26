c24a3ae2c40003f8344d93dcd0b37202c0b83ea3
huangxiang <549631030@qq.com>
2015-11-26 17:23:30 +0800

    试验单元测试

---
+++ app/src/test/java/com/hx/template/utils/TimeUtilsTest.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright &amp;copy; 2014-2016  All rights reserved.
+ *  Licensed under the 深圳中盟燧石科技 License, Version 1.0 (the "License");
+ *
+ */
+
+package com.hx.template.utils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by huangxiang on 15/11/25.
+ */
+public class TimeUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testTransformToTime1() throws Exception {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testTransformToTime2() throws Exception {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testTransformToTime3() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
