7c0b8055ac33285b7b89d5833a53f7dfe8522d2f
huangxiang <549631030@qq.com>
2015-12-28 16:59:30 +0800

    加入ScrollingActivity

--- app/src/main/java/com/hx/template/adapter/NetworkImageHolderView.java
+++ app/src/main/java/com/hx/template/adapter/NetworkImageHolderView.java
@@ -19,7 +19,7 @@ public class NetworkImageHolderView implements Holder<String> {
     public View createView(Context context) {
         //你可以通过layout文件来创建，也可以像我一样用代码创建，不一定是Image，任何控件都可以进行翻页
         imageView = new ImageView(context);
-        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         return imageView;
     }
 
